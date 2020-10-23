e3d0e9a2af2ff2f8c11e5de64cf8f25981de7306
Jason Verbeek <jason@localhost8080.org>
2020-10-23 21:40:32 +0200

    fix for parted error

--- arch-system.sh
+++ arch-system.sh
@@ -6,7 +6,7 @@ echo Setting NTP
 timedatectl set-ntp true
 
 echo Checking EFI
-ls /sys/firmware/efi/efivars 2>/dev/null
+ls /sys/firmware/efi/efivars &>/dev/null
 IS_UEFI=$?
 
 
@@ -14,19 +14,19 @@ IS_UEFI=$?
 if [ "${IS_UEFI}" -eq "2" ]; then
     echo Setting up for i386
     echo Creating root partition
-    parted /dev/sda mkpart "Arch_Drive" ext4 0% 100% 1> /dev/null
-    parted /dev/sda set 1 boot on 1> /dev/null
+    parted -s /dev/sda mkpart "Arch_Drive" ext4 0% 100% 1> /dev/null
+    parted -s /dev/sda set 1 boot on 1> /dev/null
     mkfs.ext4 /dev/sda1 1> /dev/null
     echo Mounting root partition
     mount /dev/sda1 /mnt
 else
     echo Setting up for EFI
     echo Creating EFI partition
-    parted /dev/sda mkpart "EFI_Partition" fat32 1Mib 261Mib 1> /dev/null
-    parted /dev/sda set 1 esp on 1> /dev/null
+    parted -s /dev/sda mkpart "EFI_Partition" fat32 1Mib 261Mib 1> /dev/null
+    parted -s /dev/sda set 1 esp on 1> /dev/null
     mkfs.fat -F32 /dev/sda1 1> /dev/null
     echo Creating root partition
-    parted /dev/sda mkpart "Arch_Drive" ext4 261Mib 100% 1> /dev/null
+    parted -s /dev/sda mkpart "Arch_Drive" ext4 261Mib 100% 1> /dev/null
     mkfs.ext4 /dev/sda2 1> /dev/null
     echo Mounting root partition
     mount /dev/sda2 /mnt
